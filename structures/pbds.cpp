24b5033d756abd27be80cea5c2150b439447bffa
slash0t <minenion2@gmail.com>
2025-08-06 21:46:42 +0300

    Added  pbds

---
+++ structures/pbds.cpp
@@ -0,0 +1,6 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;

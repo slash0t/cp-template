fe8dc52edf78cfdb7e126a43fb4e92ec12ecf000
slash0t <minenion2@gmail.com>
2025-08-06 21:42:38 +0300

    Added mindeque

--- structures/minqueue.cpp
+++ structures/minqueue.cpp
@@ -54,3 +54,103 @@ struct opqueue {
 		return res;
 	}
 };
+
+
+struct opdeque {
+	stack<pair<int, int>> in, out;
+	int init = 1ll << 60;
+
+	opdeque() : in(), out() {}
+
+	int size() {
+		return in.size() + out.size();
+	}
+
+	int f(int a, int b) {
+		return min(a, b);
+	}
+
+	int get(int p) {
+		if (in.size() == 0) {
+			if (out.size() == 0) return init;
+			return out.top().second;
+		}
+		if (out.size() == 0) {
+			return in.top().second;
+		}
+		int value = f(in.top().second, out.top().second);
+		return value;
+	}
+
+	void push_back(int num) {
+		int value;
+		if (in.size()) {
+			value = in.top().second;
+		} else {
+			value = init;
+		}
+		value = f(value, num);
+		in.push({ num, value });
+	}
+
+	void push_front(int num) {
+		int value;
+		if (out.size()) {
+			value = out.top().second;
+		} else {
+			value = init;
+		}
+		value = f(value, num);
+		out.push({ num, value });
+	}
+
+	void fix() {
+		bool swapped = 0;
+		if (out.size() == 0) {
+			swap(in, out);
+			swapped = 1;
+		}
+
+		int sz1 = out.size() / 2;
+		int sz2 = out.size() - sz1;
+
+		stack<int> st;
+		queue<int> q;
+		for (int i = 0; i < sz1; i++) {
+			st.push(out.top().first);
+			out.pop();
+		}
+		for (int i = 0; i < sz2; i++) {
+			q.push(out.top().first);
+			out.pop();
+		}
+		while (q.size()) {
+			int now = q.front(); q.pop();
+			push_back(now);
+		}
+		while (st.size()) {
+			int now = st.top(); st.pop();
+			push_front(now);
+		}
+
+		if (swapped) swap(in, out);
+	}
+
+	int pop_front() {
+		if (out.size() == 0) {
+			fix();
+		}
+		int res = out.top().first;
+		out.pop();
+		return res;
+	}
+
+	int pop_back() {
+		if (in.size() == 0) {
+			fix();
+		}
+		int res = in.top().first;
+		in.pop();
+		return res;
+	}
+};
